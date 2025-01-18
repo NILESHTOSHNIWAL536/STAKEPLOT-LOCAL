67f866d2543368b75e96be9b6cb8d7232d433da3
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-01-18 11:10:03 +0530

    notify

--- lib/finvu_screens/shareAccountLogin.dart
+++ lib/finvu_screens/shareAccountLogin.dart
@@ -7,7 +7,7 @@ import 'package:flutter_application_code_stakeplot/finvu_screens/linkedAccounts.
 import 'package:flutter_application_code_stakeplot/finvu_screens/otpScreen.dart';
 
 class ShareAccountLogin extends StatefulWidget {
-  const ShareAccountLogin({ Key? key }) : super(key: key);
+  const ShareAccountLogin({Key? key}) : super(key: key);
 
   @override
   _ShareAccountLoginState createState() => _ShareAccountLoginState();
@@ -16,88 +16,79 @@ class ShareAccountLogin extends StatefulWidget {
 class _ShareAccountLoginState extends State<ShareAccountLogin> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       // bottomNavigationBar: BottomNavigations(data: sizeRoom?3:2),
-       extendBody: true,
-        appBar: AppBar(
-       centerTitle: true,
-       automaticallyImplyLeading: false,
-       
-       title: Container(
-        width: MediaQuery.of(context).size.width,
-         child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-            // const Spacer(),
-                     Text(("Stakeplot"),
-                     style: FontManager().getTextStyle(context,
-                       lWeight: FontWeight.bold,
-                       fontSize: 24,
-                       color: AppColors.bg1),
-                  
-             
-                 ),
-                   
-            ],
-         ),
-       )),
+      extendBody: true,
+      appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          title: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // const Spacer(),
+                Text(
+                  ("Stakeplot"),
+                  style: FontManager().getTextStyle(context,
+                      lWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: AppColors.bg1),
+                ),
+              ],
+            ),
+          )),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        color: Colors.white,
-        // padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-                
-
-                Container(
-                     height: MediaQuery.of(context).size.height/4,
-                     width: MediaQuery.of(context).size.width/1.2,
-                ),
-                SizedBox.shrink(),
-                InkWell(
-                  onTap: (){
-                    //  Otpscreen
-                    // loginToAutoTractions(context);
-                     Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Otpscreen(),
-                      ),
+        color: Colors.amber,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 4,
+              width: MediaQuery.of(context).size.width / 1.2,
+              color: Colorcodes.barGraphOrange,
+              // decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(24),
+              //     border: Border.all(color: Colors.black)),
+            ),
+            // SizedBox.shrink(),
+            InkWell(
+                onTap: () {
+                  //  Otpscreen
+                  // loginToAutoTractions(context);
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Otpscreen(),
+                    ),
                   );
-                  },
-                  child: getButton(context,"Fetch Bank Account")
-                ),
-      
-              
-
-              
-            ],
-          ),
+                },
+                child: getButton(context, "Share Account")),
+          ],
         ),
       ),
     );
   }
 }
 
-
-
-Widget getButton(context,str){
-
-    return Container(
-                  width: MediaQuery.of(context).size.width/3,
-                  // height:MediaQuery.of(context).size.height,
-                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 20),
-                  decoration: BoxDecoration(
-                    color: Colors.cyan,
-                  ),
-                  child: Text(str),
-          );
+Widget getButton(context, str) {
+  return Container(
+    width: MediaQuery.of(context).size.width / 1.4,
+    // height:MediaQuery.of(context).size.height,
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+    decoration: BoxDecoration(
+        color: AppColors.bg6, borderRadius: BorderRadius.circular(10)),
+    child: Center(
+      child: Text(
+        str,
+        style: FontManager().getTextStyle(context,
+            lWeight: FontWeight.bold, fontSize: 24, color: Colorcodes.white),
+      ),
+    ),
+  );
 }
-
-
-
