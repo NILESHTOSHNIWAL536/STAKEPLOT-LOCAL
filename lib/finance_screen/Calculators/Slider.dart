072c36c4541fea2d89418ad12349bcf690dd43a5
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-01-28 15:08:36 +0530

    added the budget Ui screens

--- lib/finance_screen/Calculators/Slider.dart
+++ lib/finance_screen/Calculators/Slider.dart
@@ -4,21 +4,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_code_stakeplot/Home_Screen/colors.dart';
 
+
+class Slider extends StatefulWidget {
+  const Slider({ Key? key }) : super(key: key);
+
+  @override
+  _SliderState createState() => _SliderState();
+}
+
+class _SliderState extends State<Slider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+
+
 Widget buildSlider(String label, double value, double min, double max,Function(double) onChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text('$label: ₹${value.toStringAsFixed(0)}'),
-        Slider(
-          value: value,
-          min: min,
-          max: max,
-          divisions: 100,
-          label: value.toStringAsFixed(0),
-          onChanged: onChanged,
-          activeColor: AppColors.primaryColor,
-          inactiveColor: AppColors.uncoloredPie,
-        ),
+        // Slider(
+        //   value: value,
+        //   min: min,
+        //   max: max,
+        //   divisions: 100,
+        //   label: value.toStringAsFixed(0),
+        //   onChanged: onChanged,
+        //   activeColor: AppColors.primaryColor,
+        //   inactiveColor: AppColors.uncoloredPie,
+        // ),
       ],
     );
 }
