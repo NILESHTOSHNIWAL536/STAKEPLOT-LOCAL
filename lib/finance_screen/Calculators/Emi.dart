a07830b12f5b71328e260d1c752e7a407b1a75d0
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-01-28 13:25:31 +0530

    added the budget Ui screens

---
+++ lib/finance_screen/Calculators/Emi.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Emi extends StatefulWidget {
+  const Emi({ Key? key }) : super(key: key);
+
+  @override
+  _EmiState createState() => _EmiState();
+}
+
+class _EmiState extends State<Emi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/finance_screen/Calculators/Rent_Buy.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+
+class RentBuy extends StatefulWidget {
+  const RentBuy({ Key? key }) : super(key: key);
+
+  @override
+  _RentBuyState createState() => _RentBuyState();
+}
+
+class _RentBuyState extends State<RentBuy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
