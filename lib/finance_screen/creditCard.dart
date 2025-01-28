2784ce5d07042659b09598742c8190391c4c12a1
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-01-28 14:54:38 +0530

    changed ui of profile, community and finvu

--- lib/finance_screen/creditCard.dart
+++ lib/finance_screen/creditCard.dart
@@ -22,9 +22,6 @@ class _CreditCardState extends State<CreditCard> {
   double monthlyPayment = 1210;
   int monthsToPayOff = 0;
   double totalInterestPaid = 0;
-  TextEditingController cardBalanceController = TextEditingController();
-  TextEditingController interestRateController = TextEditingController();
-  TextEditingController monthlyPaymentController = TextEditingController();
 
   void calculatePayoff() {
     double balance = cardBalance;
@@ -49,9 +46,6 @@ class _CreditCardState extends State<CreditCard> {
   @override
   void initState() {
     super.initState();
-    cardBalanceController.text = cardBalance.toStringAsFixed(0);
-    interestRateController.text = interestRate.toStringAsFixed(0);
-    monthlyPaymentController.text = monthlyPayment.toStringAsFixed(0);
     calculatePayoff();
   }
 
@@ -75,72 +69,42 @@ class _CreditCardState extends State<CreditCard> {
             children: [
               Container(
                 decoration: BoxDecoration(
-                    color: AppColors.mt,
-                    borderRadius: BorderRadius.circular(16)),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Start Calculation',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 16),
-                      buildTextField('Card Balance', cardBalanceController,
-                          (value) {
-                        setState(() {
-                          cardBalance = double.tryParse(value) ?? cardBalance;
-                        });
-                        calculatePayoff();
-                      }),
-                      buildSlider('Card Balance', cardBalance, 1000, 50000,
-                          (value) {
-                        setState(() {
-                          cardBalance = value;
-                          cardBalanceController.text = value.toStringAsFixed(0);
+                  color: AppColors.mt,
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                  
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Start Calculation',
+                          style:
+                              TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 16),
+                        buildSlider('Card Balance', cardBalance, 1000, 50000,
+                            (value) {
+                          setState(() => cardBalance = value);
                           calculatePayoff();
-                        });
-                      }),
-                      buildTextField(
-                          'Interest Rate (%)', interestRateController, (value) {
-                        setState(() {
-                          interestRate = double.tryParse(value) ?? interestRate;
-                        });
-                        calculatePayoff();
-                      }),
-                      buildSlider('Interest Rate (%)', interestRate, 1, 30,
-                          (value) {
-                        setState(() {
-                          interestRate = value;
-                          interestRateController.text =
-                              value.toStringAsFixed(0);
+                        }),
+                        buildSlider('Interest Rate (%)', interestRate, 1, 30,
+                            (value) {
+                          setState(() => interestRate = value);
                           calculatePayoff();
-                        });
-                      }),
-                      buildTextField(
-                          'Monthly Payment', monthlyPaymentController, (value) {
-                        setState(() {
-                          monthlyPayment =
-                              double.tryParse(value) ?? monthlyPayment;
-                        });
-                        calculatePayoff();
-                      }),
-                      buildSlider('Monthly Payment', monthlyPayment, 500, 5000,
-                          (value) {
-                        setState(() {
-                          monthlyPayment = value;
-                          monthlyPaymentController.text =
-                              value.toStringAsFixed(0);
+                        }),
+                        buildSlider('Monthly Payment', monthlyPayment, 500, 5000,
+                            (value) {
+                          setState(() => monthlyPayment = value);
                           calculatePayoff();
-                        });
-                      }),
-                      const SizedBox(height: 20),
-                    ],
+                        }),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              
               const Text(
                 'Fetch',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
@@ -204,22 +168,6 @@ class _CreditCardState extends State<CreditCard> {
     );
   }
 
-  Widget buildTextField(String label, TextEditingController controller,
-      Function(String) onChanged) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextField(
-        controller: controller,
-        keyboardType: TextInputType.number,
-        decoration: InputDecoration(
-          labelText: label,
-          border: OutlineInputBorder(),
-        ),
-        onChanged: onChanged,
-      ),
-    );
-  }
-
   Widget buildSlider(String label, double value, double min, double max,
       Function(double) onChanged) {
     return Column(
@@ -234,8 +182,10 @@ class _CreditCardState extends State<CreditCard> {
           label: value.toStringAsFixed(0),
           onChanged: onChanged,
           activeColor: AppColors.primaryColor,
-          inactiveColor: AppColors.uncoloredPie,
+                inactiveColor: AppColors.uncoloredPie,
+          
         ),
+        
       ],
     );
   }
@@ -262,7 +212,7 @@ class _CreditCardState extends State<CreditCard> {
   }
 }
 
-// //   }
+
 // import 'package:flutter/material.dart';
 // import 'package:get/get.dart'; // Import GetX package
 // import 'package:flutter_application_code_stakeplot/Constants/app_styles.dart';
