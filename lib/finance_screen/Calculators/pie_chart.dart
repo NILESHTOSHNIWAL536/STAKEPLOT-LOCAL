1c1f3398a8b437f5251b6527d8d903f0ab718607
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-01-28 17:27:48 +0530

    changed ui of profile, community and finvu

--- lib/finance_screen/Calculators/pie_chart.dart
+++ lib/finance_screen/Calculators/pie_chart.dart
@@ -1,37 +1,37 @@
-// import 'package:fl_chart/fl_chart.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_code_stakeplot/';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_code_stakeplot/';
 
-// class PieChart extends StatefulWidget {
-//   const PieChart({super.key});
+class PieChartgraph extends StatefulWidget {
+  const PieChart({super.key});
 
-//   @override
-//   State<PieChart> createState() => _PieChartState();
-// }
+  @override
+  State<PieChartgraph> createState() => _PieChartState();
+}
 
-// class _PieChartState extends State<PieChart> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return buildPieChart();
-//   }
-// }
-// Widget buildPieChart() {
-//     return PieChart(
-//       PieChartData(
-//         sections: [
-//           PieChartSectionData(
-//             value: cardBalance,
-//             title: 'Principal\n₹${cardBalance.toStringAsFixed(0)}',
-//             color: AppColors.primaryColor,
-//             radius: 50,
-//           ),
-//           PieChartSectionData(-
-//             value: totalInterestPaid,
-//             title: 'Interest\n₹${totalInterestPaid.toStringAsFixed(0)}',
-//             color: AppColors.uncoloredPie,
-//             radius: 50,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
+class _PieChartState extends State<PieChart> {
+  @override
+  Widget build(BuildContext context) {
+    return buildPieChart();
+  }
+}
+Widget buildPieChart() {
+    return PieChart(
+      PieChartData(
+        sections: [
+          PieChartSectionData(
+            value: cardBalance,
+            title: 'Principal\n₹${cardBalance.toStringAsFixed(0)}',
+            color: AppColors.primaryColor,
+            radius: 50,
+          ),
+          PieChartSectionData(-
+            value: totalInterestPaid,
+            title: 'Interest\n₹${totalInterestPaid.toStringAsFixed(0)}',
+            color: AppColors.uncoloredPie,
+            radius: 50,
+          ),
+        ],
+      ),
+    );
+  }
