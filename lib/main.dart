a2b9362d1bf1be2f4b5abbb1dd863e401eb77e91
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-01-30 10:57:40 +0530

    added the budget Ui screens

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class _MyAppState extends State<MyApp> {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-       initialRoute: '/PlotFinance', 
+       initialRoute: '/', 
         routes:
     {  
       '/': (context) =>  HomePage(),   
