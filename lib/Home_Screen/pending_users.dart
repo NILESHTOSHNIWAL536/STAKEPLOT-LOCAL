204bb6334b0e4653f890708bb125e338119d7713
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-02-18 18:43:09 +0530

    corrections

--- lib/Home_Screen/pending_users.dart
+++ lib/Home_Screen/pending_users.dart
@@ -7,6 +7,7 @@ import 'package:flutter_application_code_stakeplot/colorcodes.dart';
 import 'package:flutter_application_code_stakeplot/profile.dart';
 import 'package:get/get.dart';
 import './colors.dart';
+import 'package:intl/intl.dart';
 
 class UserListScreen extends StatefulWidget {
   @override
@@ -54,13 +55,11 @@ class _UserListScreenState extends State<UserListScreen> {
             ],
           ),
         ),
-        
         Obx(() => getlendUsers.value ? getUser() : getUser()),
       ],
     );
   }
 
-  
 //    Widget getUser() {
 //   return Container(
 //     width: MediaQuery.of(context).size.width / 1.1,
@@ -104,16 +103,16 @@ class _UserListScreenState extends State<UserListScreen> {
 //     ),
 //   );
 // }
-Widget getUser() {
+  Widget getUser() {
     return Visibility(
-      visible: lendAmountRemainders.isNotEmpty, // Show only if there are pending dues
+      visible: lendAmountRemainders
+          .isNotEmpty, // Show only if there are pending dues
       child: Container(
         width: MediaQuery.of(context).size.width / 1.1,
         padding: EdgeInsets.symmetric(vertical: 6),
-        
         child: Column(
           children: lendAmountRemainders
-              .take(3) // Show up to 3 users
+              .take(2) // Show up to 3 users
               .map((data) => ListTile(
                     leading: CircleAvatar(
                         backgroundColor: Colorcodes.budgetLightGreen,
@@ -159,50 +158,136 @@ class ShowAllUsersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-        appBar: AppBar(
-          backgroundColor: AppColors.backgroundColor,
-          title: Text('Dues to receive',
-              style: FontManager().getTextStyle(context,
-                  lWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: AppColors.accentColor)),
-        ),
-        body: Expanded(
-          child: ListView.builder(
-            itemCount: lendAmountRemainders.length,
-            itemBuilder: (context, index) {
-              var data = lendAmountRemainders[index];
-              return ListTile(
-                leading: CircleAvatar(
-                    backgroundColor: Colorcodes.budgetLightGreen,
-                    child: ProfileImage(
-                        url: data['Avatar'] ?? 'assets/avatar/menp4.svg')),
-                title: Text(data["userName"] ?? "Unknown User", style:  FontManager().getTextStyle(context,
-                  lWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: AppColors.accentColor),),
-                trailing: InkWell(
-                  onTap: () {
-                    sendNotificationsToDevice(data['_id'], context,
-                        "You Need To Pay Lend To ${userName.value} of ${data['amount'] ?? "0000"}");
-                    // print(currentId.value);
-                  },
-                  child: Text(
-                    (data["billApproved"] ?? true)
-                        ? "Remind now"
-                        : "Didn't approve",
-                    style:
-                         FontManager().getTextStyle(context,
-                  lWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: AppColors.primaryColor),
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor,
+        title: Text('Payable overview',
+            style: FontManager().getTextStyle(context,
+                lWeight: FontWeight.bold,
+                fontSize: 18,
+                color: AppColors.accentColor)),
+      ),
+      body: DefaultTabController(
+        length: 2, // Number of tabs
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+              child: DecoratedBox(
+                decoration: const BoxDecoration(),
+                child: TabBar(
+                  indicator: BoxDecoration(
+                    // Rounded corners
+
+                    color: AppColors.tab,
+
+                    borderRadius: BorderRadius.circular(16),
                   ),
+                  // Padding for labels
+                  labelColor:
+                      AppColors.primaryColor, // Text color for selected tab
+                  unselectedLabelColor:
+                      AppColors.bg1, // Text color for unselected tabs
+
+                  tabs: const [
+                    Tab(child: Text('Payable')),
+                    Tab(text: 'Owed'),
+                    // Tab(text: 'Exploria'),
+                  ],
                 ),
-              );
-            },
-          ),
-        ));
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height /
+                  1.609, // Adjust as needed for TabBarView
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 12.0),
+                child: TabBarView(
+                  children: [
+                    Center(child: Userslist()),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
+Widget Userslist() {
+  return Expanded(
+    child: ListView.builder(
+      itemCount: lendAmountRemainders.length,
+      itemBuilder: (context, index) {
+        var data = lendAmountRemainders[index];
+        print("data mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+        print(data);
+        return ListTile(
+          leading: CircleAvatar(
+              backgroundColor: Colorcodes.budgetLightGreen,
+              child: ProfileImage(
+                  url: data['Avatar'] ?? 'assets/avatar/menp4.svg')),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    data["userName"] ?? "Unknown User",
+                    style: FontManager().getTextStyle(context,
+                        lWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: AppColors.accentColor),
+                  ),
+                  Text(
+                    DateFormat('dd MMM yyyy hh:mm a')
+                        .format(DateTime.parse(data["createdAt"])),
+                    style: FontManager().getTextStyle(context,
+                        lWeight: FontWeight.w300,
+                        fontSize: 8,
+                        color: AppColors.accentColor),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    data["name"] ?? "Untagged",
+                    style: FontManager().getTextStyle(context,
+                        lWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: AppColors.accentColor),
+                  ),
+                  Text(
+  '₹${NumberFormat.currency(symbol: '₹', decimalDigits: 2).format(data["amount"])}',
+  style: FontManager().getTextStyle(context,
+      lWeight: FontWeight.bold,
+      fontSize: 12,
+      color: AppColors.accentColor),
+)
+                ],
+              ),
+            ],
+          ),
+          trailing: InkWell(
+            onTap: () {
+              sendNotificationsToDevice(data['_id'], context,
+                  "You Need To Pay Lend To ${userName.value} of ${data['amount'] ?? "0000"}");
+              // print(currentId.value);
+            },
+            child: Text(
+              (data["billApproved"] ?? true) ? "Remind now" : "Didn't approve",
+              style: FontManager().getTextStyle(context,
+                  lWeight: FontWeight.bold,
+                  fontSize: 12,
+                  color: AppColors.primaryColor),
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
 //
