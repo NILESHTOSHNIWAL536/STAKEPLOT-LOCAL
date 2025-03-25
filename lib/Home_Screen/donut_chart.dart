6e9288093a2b90b1e2be9ec68f12ad9a0180c465
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-03-25 17:48:04 +0530

    corrections

--- lib/Home_Screen/donut_chart.dart
+++ lib/Home_Screen/donut_chart.dart
@@ -418,15 +418,25 @@ class _DoughnutChartExampleState extends State<DoughnutChartExample> {
                                 dataLabelSettings:
                                     const DataLabelSettings(isVisible: false),
                                 enableTooltip: true,
-                                onPointTap: (ChartPointDetails details) {
+                                // onPointTap: (ChartPointDetails details) {
                                   
-                                    if (selectedIndex.value == details.pointIndex) {
-                                      selectedIndex.value = -1;
-                                    } else {
-                                      selectedIndex.value = details.pointIndex!;
-                                    }
+                                //     if (selectedIndex.value == details.pointIndex) {
+                                //       selectedIndex.value = -1;
+                                //     } else {
+                                //       selectedIndex.value = details.pointIndex!;
+                                //     }
                                  
-                                },
+                                // },
+                                onPointTap: (ChartPointDetails details) {
+  if (chartData.isNotEmpty && details.pointIndex != null && details.pointIndex! < chartData.length) {
+    if (selectedIndex.value == details.pointIndex) {
+      selectedIndex.value = -1;
+    } else {
+      selectedIndex.value = details.pointIndex!;
+    }
+  }
+},
+
                               ),
                             ],
                             
