5fe97077bc26e239e7e7c0cfcc3be7d022ef4567
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-03-27 16:49:48 +0530

    change in the logout functionality loader...

--- lib/finvu_screens/appbar_widget.dart
+++ lib/finvu_screens/appbar_widget.dart
@@ -8,12 +8,23 @@ import 'package:flutter_application_code_stakeplot/colorcodes.dart';
 AppBar getAppBar(context) {
   return AppBar(
      toolbarHeight: 40,
+     actions: [ 
+       Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 20),
+         child: Icon(
+            Icons.login,
+            color: Colorcodes.black,
+            size: 30,
+         ),
+       ),
+     ],
     leading: IconButton(
       icon: Icon(Icons.arrow_back_sharp),
       color: Colorcodes.black,
       onPressed: () {
         Navigator.pop(context);
       },
+
     ),
   );
 }
