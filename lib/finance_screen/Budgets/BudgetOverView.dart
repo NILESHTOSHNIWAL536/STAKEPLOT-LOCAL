232f29555b56e492337b87c780ee7457f28046dd
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-05-21 12:52:45 +0530

    Foodie funds checks done and loading

--- lib/finance_screen/Budgets/BudgetOverView.dart
+++ lib/finance_screen/Budgets/BudgetOverView.dart
@@ -8,6 +8,7 @@ import 'package:flutter_application_code_stakeplot/bottomNavigations.dart';
 import 'package:flutter_application_code_stakeplot/colorcodes.dart';
 import 'package:flutter_application_code_stakeplot/finance_screen/Budgets/Budget.dart';
 import 'package:flutter_application_code_stakeplot/finvu_screens/shareAccountLogin.dart';
+import 'package:flutter_application_code_stakeplot/loader.dart';
 import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
@@ -31,6 +32,7 @@ class BudgetOverView extends StatefulWidget {
 
 class _BudgetOverViewState extends State<BudgetOverView> {
   Map<String, double> updatedAmounts = {};
+  bool _isProcessing = false;
   @override
   void initState() {
     super.initState();
@@ -64,7 +66,6 @@ class _BudgetOverViewState extends State<BudgetOverView> {
   }
 
   Widget getBudgetUiScreen(double height, double width) {
-   
     return Container(
       width: width,
       height: height,
@@ -109,18 +110,17 @@ class _BudgetOverViewState extends State<BudgetOverView> {
               ),
             ),
             Padding(
-                    padding: EdgeInsets.only(
-                        bottom: Colorcodes.paddingSize),),
+              padding: EdgeInsets.only(bottom: Colorcodes.paddingSize),
+            ),
             Row(
               children: [
-                
                 textStyle(
                     context: context,
                     text: "${widget.period}",
                     fontsize: 16,
                     fontWeight: FontWeight.w500,
                     c: AppColors.accentColor),
-                    textStyle(
+                textStyle(
                     context: context,
                     text: " Estimation",
                     fontsize: 16,
@@ -135,9 +135,15 @@ class _BudgetOverViewState extends State<BudgetOverView> {
               height: Colorcodes.paddingCard / 2,
             ),
             InkWell(
-                onTap: () {
-                  addBudget(context, widget.name, widget.amount,
+                onTap: () async {
+                  // Show loader
+                
+                  // Add budget
+                   addBudget(context, widget.name, widget.amount,
                       categoriesDividedList, widget.period);
+
+                  // Dismiss loader
+                 
                 },
                 child: getButton(context, "Add Budget")),
           ],
@@ -147,98 +153,98 @@ class _BudgetOverViewState extends State<BudgetOverView> {
   }
 
   Widget categoryList() {
-  return Container(
-  
-    width: MediaQuery.of(context).size.width,
-    height: MediaQuery.of(context).size.height / 1.55,
-    child: SingleChildScrollView(
-      child: Column(
-        children: List.generate(categoriesDividedList.length, (index) {
-          String urlAvatar = "";
-          try {
-            urlAvatar = Categories.link +
-                BudgetCategories2.listofCategories[
-                    categoriesDividedList[index]['category']];
-          } catch (e) {
-            urlAvatar = Categories.link +
-                BudgetCategories2.listofCategories['Entertainment'];
-          }
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 1.55,
+      child: SingleChildScrollView(
+        child: Column(
+          children: List.generate(categoriesDividedList.length, (index) {
+            String urlAvatar = "";
+            try {
+              urlAvatar = Categories.link +
+                  BudgetCategories2.listofCategories[
+                      categoriesDividedList[index]['category']];
+            } catch (e) {
+              urlAvatar = Categories.link +
+                  BudgetCategories2.listofCategories['Entertainment'];
+            }
 
-          return Column(
-            children: [
-              Container(
-                //padding: EdgeInsets.symmetric(horizontal: 7),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: AvatarProfileImage(
-                        url: urlAvatar,
-                        width: 26,
-                        height: 28,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Text(
-                        categoriesDividedList[index]['category']!,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
+            return Column(
+              children: [
+                Container(
+                  //padding: EdgeInsets.symmetric(horizontal: 7),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: AvatarProfileImage(
+                          url: urlAvatar,
+                          width: 26,
+                          height: 28,
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: TextField(
-                        controller: TextEditingController(
-                          text: categoriesDividedList[index]['amount']
-                              .toString(),
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          categoriesDividedList[index]['category']!,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                        decoration: InputDecoration(
-
-                        //  prefixIcon: Icon(Icons.currency_rupee),
-                          contentPadding: EdgeInsets.symmetric(
-                              vertical: 0, horizontal: 4), // Removes extra spacing
-                          isDense: true, // Reduces extra height
-                          hintText: "Enter amount",
-                          hintStyle: FontManager().getTextStyle(context,
-            lWeight: FontWeight.w400, fontSize: 12, color: AppColors.accentColor),
-                          errorText: _isAmountExceeded(index)
-                              ? "Amount exceeds budget"
-                              : null,
-                        ),
-                        keyboardType: TextInputType.number,
-                        onSubmitted: (value) {
-                          if (_validateAmount(value, widget.amount)) {
-                            onsubmit(index, value);
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Amount exceeds the total budget!"),
-                                backgroundColor: Colors.red,
-                              ),
-                            );
-                          }
-                        },
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          controller: TextEditingController(
+                            text: categoriesDividedList[index]['amount']
+                                .toString(),
+                          ),
+                          decoration: InputDecoration(
+                            //  prefixIcon: Icon(Icons.currency_rupee),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 0,
+                                horizontal: 4), // Removes extra spacing
+                            isDense: true, // Reduces extra height
+                            hintText: "Enter amount",
+                            hintStyle: FontManager().getTextStyle(context,
+                                lWeight: FontWeight.w400,
+                                fontSize: 12,
+                                color: AppColors.accentColor),
+                            errorText: _isAmountExceeded(index)
+                                ? "Amount exceeds budget"
+                                : null,
+                          ),
+                          keyboardType: TextInputType.number,
+                          onSubmitted: (value) {
+                            if (_validateAmount(value, widget.amount)) {
+                              onsubmit(index, value);
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content:
+                                      Text("Amount exceeds the total budget!"),
+                                  backgroundColor: Colors.red,
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 10), // Adds space between rows
-            ],
-          );
-        }),
+                SizedBox(height: 10), // Adds space between rows
+              ],
+            );
+          }),
+        ),
       ),
-    ),
-  );
-}
-
+    );
+  }
 
   void onsubmit(index, value) async {
-  
     categoriesDividedList[index]['amount'] = double.tryParse(value) ?? 0;
 
     var d = await adjustBudget(double.parse(widget.amount),
@@ -254,61 +260,58 @@ class _BudgetOverViewState extends State<BudgetOverView> {
 
     categoriesDividedList.clear();
     categoriesDividedList.addAll(List.from(categoryList));
-    
   }
-
 
   Future<Map<String, double>> adjustBudget(
     double totalAmount,
     String updatedCategory,
     double updatedAmount,
     List<String> selectedCategories,
-    ) async {
-   updatedAmounts[updatedCategory]=updatedAmount;
-  // Step 1: Calculate the total weight of selected categories
-  Map<String, double> subcategoryWeights = {};
-  categoryWeights.forEach((mainCategory, data) {
-    data["subcategories"].forEach((subCategory, weight) {
-      subcategoryWeights[subCategory] = weight.toDouble();
+  ) async {
+    updatedAmounts[updatedCategory] = updatedAmount;
+    // Step 1: Calculate the total weight of selected categories
+    Map<String, double> subcategoryWeights = {};
+    categoryWeights.forEach((mainCategory, data) {
+      data["subcategories"].forEach((subCategory, weight) {
+        subcategoryWeights[subCategory] = weight.toDouble();
+      });
     });
-  });
 
-  Map<String, double> selectedWeights = {
-    for (var category in selectedCategories)
-      if (subcategoryWeights.containsKey(category))
-        category: subcategoryWeights[category]!
-  };
-  
-  // //updatedAmounts[updatedCategory]=updatedAmount;
-  // double totalSelectedWeight = selectedWeights.values.fold(0, (a, b) => (a + b));
+    Map<String, double> selectedWeights = {
+      for (var category in selectedCategories)
+        if (subcategoryWeights.containsKey(category))
+          category: subcategoryWeights[category]!
+    };
 
-  double totalSelectedWeight = selectedWeights.entries
-      .where((entry) => !updatedAmounts.containsKey(entry.key))
-      .fold(0, (sum, entry) => sum + entry.value);
-  
-  
-   
-  
-  // Step 2: Calculate the remaining budget after updated values
-  double usedBudget = updatedAmounts.values.fold(0, (a, b) => a + b);
-  double remainingBudget = totalAmount - usedBudget;
+    // //updatedAmounts[updatedCategory]=updatedAmount;
+    // double totalSelectedWeight = selectedWeights.values.fold(0, (a, b) => (a + b));
 
-  // Step 3: Distribute remaining budget proportionally based on category weights
-  Map<String, double> finalBudgets = {};
-  selectedWeights.forEach((subCategory, weight) {
-    if (!updatedAmounts.containsKey(subCategory)) {
-      finalBudgets[subCategory] = ((remainingBudget * weight) / totalSelectedWeight)
-              .clamp(0, double.infinity);
-    } else {
-      finalBudgets[subCategory] = updatedAmounts[subCategory]!; // Keep previous updates
-    }
-  });
+    double totalSelectedWeight = selectedWeights.entries
+        .where((entry) => !updatedAmounts.containsKey(entry.key))
+        .fold(0, (sum, entry) => sum + entry.value);
 
-  // Step 4: Round all amounts to 2 decimal places
-  finalBudgets.updateAll((key, value) => (value * 100).roundToDouble() / 100);
+    // Step 2: Calculate the remaining budget after updated values
+    double usedBudget = updatedAmounts.values.fold(0, (a, b) => a + b);
+    double remainingBudget = totalAmount - usedBudget;
 
-  return finalBudgets;
-}
+    // Step 3: Distribute remaining budget proportionally based on category weights
+    Map<String, double> finalBudgets = {};
+    selectedWeights.forEach((subCategory, weight) {
+      if (!updatedAmounts.containsKey(subCategory)) {
+        finalBudgets[subCategory] =
+            ((remainingBudget * weight) / totalSelectedWeight)
+                .clamp(0, double.infinity);
+      } else {
+        finalBudgets[subCategory] =
+            updatedAmounts[subCategory]!; // Keep previous updates
+      }
+    });
+
+    // Step 4: Round all amounts to 2 decimal places
+    finalBudgets.updateAll((key, value) => (value * 100).roundToDouble() / 100);
+
+    return finalBudgets;
+  }
 
   Future<Map<String, double>> adjustBudget2(
       double totalAmount,
