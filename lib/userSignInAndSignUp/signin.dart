71a30f0981b43910394d03f94e5a3ebd705e9277
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-08-01 16:19:24 +0530

    changes in the hive

--- lib/userSignInAndSignUp/signin.dart
+++ lib/userSignInAndSignUp/signin.dart
@@ -32,9 +32,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController emailController = TextEditingController(text: "nileshtoshniwal743@gmail.com");
+  final TextEditingController emailController = TextEditingController(text: "");
   final TextEditingController passwordController =
-      TextEditingController(text: "Nilesh1234@");
+      TextEditingController(text: "");
   bool _isPasswordVisible = false;
   final AuthService authService = AuthService();
   @override
