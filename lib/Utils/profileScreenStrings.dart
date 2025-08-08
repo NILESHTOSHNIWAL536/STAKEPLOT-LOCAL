019aaf5f4eae2b6d85931fb55c45ef8725255102
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-08-08 15:15:08 +0530

    chnages in the coupon code

--- lib/Utils/profileScreenStrings.dart
+++ lib/Utils/profileScreenStrings.dart
@@ -22,7 +22,7 @@ class ProfileScreenStrings {
   String historyArchivesLabel = "History archives";
   String termsConditionsLabel = "Terms & conditions";
   String logoutLabel = "Log out";
-  String appVersionLabel = "Stakeplot\nApp version 4.0.0";
+  String appVersionLabel = "Stakeplot\nApp version 4.3.0";
 
   // ✅ Sublabels
   String communityProfileSubLabel = "Check your community profile";
