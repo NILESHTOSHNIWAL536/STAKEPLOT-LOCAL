ca3dda0245e108a65be4f4a2986e5066ce4965dc
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2025-09-01 10:40:18 +0530

    chnages in the share data

---
+++ lib/backed_connections/apiAutomations/share_data.dart
@@ -0,0 +1,62 @@
+import 'package:share_plus/share_plus.dart';
+
+
+
+import 'package:share_plus/share_plus.dart';
+
+void shareBankData(Map<String, dynamic> data) {
+  // Format the bank info
+  final String bankInfo = """
+Bank Details:
+---------------------
+Bank ID: ${data['bankId']}
+Bank Name: ${data['bankName']}
+Bank Logo: ${data['bankLogo']}
+FIP ID: ${data['fipId']}
+Account ID: ${data['accountId']}
+Account Number: ${data['maskedAccNumber']}
+Type: ${data['type']}
+Current Balance: ₹${data['currentBalance']}
+Last Fetch: ${data['lastFetch']}
+Next Fetch: ${data['nextFetch']}
+Fetch Count: ${data['fetchCount']}
+
+User Info:
+---------------------
+Name: ${data['name']}
+PAN: ${data['pan']}
+DOB: ${data['dob']}
+Mobile: ${data['mobile']}
+${(data['address'] != null && 
+   !data['address'].toString().toLowerCase().contains("encrypted"))
+   ? "User Address: ${data['address']}" 
+   : ""}
+Branch Address:  ${data['branchAddress']}
+ifscCode:  ${data['ifscCode']}
+
+""";
+
+
+final String bankInfo2 = """
+Bank Details:
+---------------------
+Name: ${data['name']}
+Bank Name: ${data['bankName']}
+Account Number: ${data['maskedAccNumber']}
+IFSC Code: ${data['ifscCode']}
+Branch Address: ${data['branchAddress']}
+${(data['address'] != null && 
+   !data['address'].toString().toLowerCase().contains("encrypted"))
+   ? "User Address: ${data['address']}" 
+   : ""}
+Branch Address:  ${data['branchAddress']}
+""";
+
+  // Share outside the app
+  try{
+  Share.share(bankInfo2, subject: "My Bank Account Info");
+  }catch(e)
+  {
+    print(e);
+  }
+}
