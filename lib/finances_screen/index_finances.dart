d4b25a235e3e3aef274be0b9d14adde1f9981912
krishnamanohar0987 <krishnanaruto362@gmail.com>
2025-09-03 18:02:38 +0530

    Budget calci ui

--- lib/finances_screen/index_finances.dart
+++ lib/finances_screen/index_finances.dart
@@ -118,7 +118,6 @@ class _FinanceDashboardState extends State<FinanceDashboard> {
             ),
 
             // Middle sections start here
-            const SizedBox(height: 24),
 
             // "Heading" section with "View all"
             Padding(
