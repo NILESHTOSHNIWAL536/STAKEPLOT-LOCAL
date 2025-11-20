43109306ae8ba4af3ec33de68910e1c81555e241
Roshanchenna <roshanchenna@gmail.com>
2025-11-20 16:19:41 +0530

    removed the console logs

--- src/config/queue-config.js
+++ src/config/queue-config.js
@@ -1,10 +1,5 @@
 const { Queue } = require("bullmq");
 
-console.log("Queue Config Loaded");
-console.log("REDIS_HOST:", process.env.REDIS_HOST);
-console.log("REDIS_PORT:", process.env.REDIS_PORT);
-console.log("REDIS_PASSWORD:", process.env.REDIS_PASSWORD);
-
 const REDIS_HOST = process.env.REDIS_HOST;
 const REDIS_PORT = Number(process.env.REDIS_PORT);
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
