0f1a756fd419e0f19dad5f55b7d3a81f19ed503a
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2026-04-10 17:59:18 +0530

    updated the manual transactions with the collections linking code

--- lib/Home_Screen/history/transactions_ui_component.dart
+++ lib/Home_Screen/history/transactions_ui_component.dart
@@ -17,6 +17,7 @@ import '../../Constants/core/app_padding_sizes.dart';
 import '../../components/shared_utils.dart';
 import '../../repository/transactions_repository.dart';
 import '../ManuallyTransactions/collections_manualtransactions.dart';
+import 'CollectionsSplitBottomSheet.dart';
 
 RxMap<String, String> redioButton = <String, String>{}.obs;
 RxMap<String, int> redioButtonIndex = <String, int>{}.obs;
@@ -236,21 +237,11 @@ Future<dynamic> showCustomFriendsModalTransactionHistory(BuildContext context,
       borderRadius: BorderRadius.vertical(top: Radius.circular(18)),
     ),
     builder: (BuildContext context) {
-      return CollectionsManualtransactions(
+      return CollectionsSplitBottomSheet(
         amount: amount,
         transactionId: transactionid,
       );
-      // return NewFriendsUi(
-      //   totalAmount: amount.toDouble(),
-      //   userId: userController.userId.value,
-      //   userName: userController.userName.value,
-      //   userAvatar: userController.avatar.value,
-      //   isLendMode: isLendMode,
-      //   category: category,
-      //   subcategory: subcategory,
-      //   flag: true,
-      //   ismanual: false,
-      // );
+
     },
   );
 }
