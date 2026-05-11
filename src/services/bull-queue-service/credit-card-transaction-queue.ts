d52ac3029b86d91a1519afda97970055aa8ceb50
Nilesh Toshniwal <nilesh.toshniwal@braneenterprises.com>
2026-05-11 12:47:44 +0530

    updated the code for the error of semgrep

--- src/services/bull-queue-service/credit-card-transaction-queue.ts
+++ src/services/bull-queue-service/credit-card-transaction-queue.ts
@@ -4,11 +4,10 @@ console.log(' Worker started (bull queue)');
 
 CreditCardQueue.process(async (job) => {
   const { userId, emailBankMap } = job.data;
-  console.log(`Processing user ${userId}`);
-
   try {
     for (const item of emailBankMap) {
       const { email, bankIds } = item;
+      await EmailScrapingService.scrapeEmailsByBankId(userId.toString(), bankIds, email);
     }
   } catch (error) {
     throw error;
